af6b08a69ea17e3daafe115b288c9a4ab911e292
TheAnushree25 <theanushree25@gmail.com>
2025-12-14 15:27:24 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -159,6 +159,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
